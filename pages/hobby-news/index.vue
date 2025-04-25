c137eb9a6c8b835940a8a0517c178c0b3b49244c
Imna29 <gio.imnaishvili029@gmail.com>
2025-04-25 19:38:42 +0400

    get rid of cat

--- pages/hobby-news/index.vue
+++ pages/hobby-news/index.vue
@@ -5,16 +5,7 @@
 <template>
   <div>
     <div class="text-6xl text-center mt-10">
-      Work in progress
-    </div>
-    <div>
-      <img src="public/work.gif" class="w-1/3 mx-auto mt-2" />
-    </div>
-    <div class="text-2xl text-center mt-2">
-      This is how Tolia builds websites
+      Coming soon!
     </div>
   </div>
 </template>
-
-
-<style></style>
